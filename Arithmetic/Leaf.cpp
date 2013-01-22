8c83daeaf496f28d0630f6ddfacf3315651f6ad0
Erik Larsson <erikl3@kth.se>
2013-01-22 22:32:04 +0100

    Added structure for adding and multiplying functions

--- Arithmetic/Leaf.cpp
+++ Arithmetic/Leaf.cpp
@@ -6,13 +6,16 @@ Leaf::Leaf(void) : BaseNode(BaseNode::LEAF)
 {
 }
 
-
 Leaf::~Leaf(void)
 {
 }
 
 std::vector<char> &Leaf::getData(void) {
 	return data;
+}
+
+const std::vector<char> &Leaf::getData(void) const {
+        return data;
 }
 
 char &Leaf::operator[](int32_t index) {
@@ -34,3 +37,48 @@ const char &Leaf::operator[](int32_t index) const {
 int32_t Leaf::getLength(void) const {
 	return data.size();
 };
+
+
+Leaf &Leaf::operator=(const Leaf &leaf)
+{
+        if(this == &leaf)
+    	    return *this;
+
+        data = leaf.getData();
+
+        return *this;
+}
+
+Leaf & Leaf::operator+=(const Leaf & leaf) 
+{
+        // Replace with real code, with big integers
+        data[0]+=leaf[0];
+
+        return *this;
+}
+
+Leaf Leaf::operator+(const Leaf & leaf) const 
+{
+        return Leaf(*this) += leaf;
+}
+
+Leaf & Leaf::operator*=(const Leaf & leaf) 
+{
+        // Replace with real code, with big integers
+        data[0]*=leaf[0];
+
+        return *this;
+}
+
+Leaf Leaf::operator*(const Leaf & leaf) const 
+{
+        return Leaf(*this) *= leaf;
+}
+
+bool Leaf::operator==(const Leaf &leaf) const
+{
+        if(data == leaf.getData())
+	    return true;
+
+        return false;
+}
--- Arithmetic/Leaf.h
+++ Arithmetic/Leaf.h
@@ -13,8 +13,20 @@ public:
 	char &operator[](int32_t index);
 	const char &operator[](int32_t index) const;
 	std::vector<char> &getData(void);
+	const std::vector<char> &getData(void) const;
 
 	virtual int32_t getLength(void) const;
+
+	Leaf &operator=(const Leaf &leaf);
+
+	Leaf &operator+=(const Leaf &leaf);
+	Leaf operator+(const Leaf &leaf) const;
+
+	Leaf &operator*=(const Leaf &leaf);
+	Leaf operator*(const Leaf &leaf) const;
+	
+	bool operator==(const Leaf &leaf) const;
+
 private:
 	std::vector<char> data;
 };
