997308942a283783a67c761aa0a209e56b6d449c
Erik Larsson <erikl3@kth.se>
2013-01-23 20:58:35 +0100

    Added addition and multiplication functions, added toString function

--- Arithmetic/IntLeaf.cpp
+++ Arithmetic/IntLeaf.cpp
@@ -1,8 +1,42 @@
 #include "IntLeaf.h"
 
+#define ARRAYORDER 1 /* -1 for least significant byte first, 1 for most significant byte first */
+#define ENDIAN 0
+#define NAILS 0
 
 IntLeaf::IntLeaf(void)
 {
+}
+
+IntLeaf::IntLeaf(std::vector<char> bytevec)
+{
+    bool negative = false;
+
+    /* Check if negative - if signed bit is 1 */
+    if(ARRAYORDER == 1)
+	negative = bytevec[0] & 0x80;
+    else
+	negative = bytevec[bytevec.size()-1] & 0x80;
+
+    if(negative) 
+    {
+	/* Change to corresponding positive number using two's complement */
+	for(unsigned int i=0; i < bytevec.size(); i++)
+	{
+	    bytevec[i] ^= 0xFF;
+	}
+
+	if(ARRAYORDER == 1)
+	    bytevec[bytevec.size() - 1]++;
+	else
+	    bytevec[0]++;
+    }
+
+    mpz_import(data.get_mpz_t(), bytevec.size(), ARRAYORDER, 
+	       sizeof(bytevec[0]), ENDIAN, NAILS, bytevec.data());
+
+    if(negative)
+	mpz_neg(data.get_mpz_t(), data.get_mpz_t());
 }
 
 
@@ -10,6 +44,94 @@ IntLeaf::~IntLeaf(void)
 {
 }
 
-std::vector<char> IntLeaf::toVector(void) const {
-	return std::vector<char>();
+int32_t IntLeaf::getLength(void) const
+{
+    return mpz_sizeinbase(data.get_mpz_t(), 256);
 }
+
+std::vector<char> IntLeaf::toVector(void) const 
+{
+    unsigned int size = mpz_sizeinbase(data.get_mpz_t(), 256);
+    std::vector<char> bytevec(size);
+
+    mpz_export(bytevec.data(), NULL, ARRAYORDER, sizeof(bytevec[0]), ENDIAN, NAILS, data.get_mpz_t());
+
+    /* Check if negative */
+    bool negative = (mpz_sgn(data.get_mpz_t()) == -1);
+
+    if(negative)
+    {
+	/* Change to corresponding negative number using two's complement */
+	for(unsigned int i=0; i < bytevec.size(); i++)
+	{
+	    bytevec[i] ^= 0xFF;
+	}
+
+	if(ARRAYORDER == 1)
+	    bytevec[bytevec.size() - 1]++;
+	else
+	    bytevec[0]++;
+    }
+
+    return bytevec;
+}
+
+
+IntLeaf & IntLeaf::addTo(const IntLeaf & leaf)
+{
+    data += leaf.data;
+
+    return *this;
+}
+
+
+IntLeaf IntLeaf::add(const IntLeaf & leaf) const
+{
+    return IntLeaf(*this) += leaf;
+}
+
+
+IntLeaf & IntLeaf::operator+=(const IntLeaf & leaf)
+{
+    return addTo(leaf);
+}
+
+
+IntLeaf IntLeaf::operator+(const IntLeaf & leaf) const
+{
+    return add(leaf);
+}
+
+
+IntLeaf & IntLeaf::multTo(const IntLeaf & leaf)
+{
+    data *= leaf.data;
+
+    return *this;
+}
+
+
+IntLeaf IntLeaf::mult(const IntLeaf & leaf) const
+{
+    return IntLeaf(*this) += leaf;
+}
+
+
+IntLeaf & IntLeaf::operator*=(const IntLeaf & leaf)
+{
+    return multTo(leaf);
+}
+
+
+IntLeaf IntLeaf::operator*(const IntLeaf & leaf) const
+{
+    return mult(leaf);
+}
+
+
+
+std::string IntLeaf::toString(void) const
+{
+    return data.get_str();
+}
+
--- Arithmetic/IntLeaf.h
+++ Arithmetic/IntLeaf.h
@@ -1,16 +1,31 @@
 #pragma once
-#include "baseleaf.h"
+#include "BaseLeaf.h"
+
+#include <gmp.h>
+#include <gmpxx.h>
+
+#include <string>
+
 class IntLeaf :
 	public BaseLeaf
 {
 public:
 	IntLeaf(void);
+	explicit IntLeaf(std::vector<char> bytevec);
 	~IntLeaf(void);
+
+	IntLeaf &add(const IntLeaf &leaf);
 
 	IntLeaf &operator=(const IntLeaf &leaf);
 
+	IntLeaf &addTo(const IntLeaf &leaf);
+	IntLeaf add(const IntLeaf &leaf) const;
+
 	IntLeaf &operator+=(const IntLeaf &leaf);
 	IntLeaf operator+(const IntLeaf &leaf) const;
+
+	IntLeaf &multTo(const IntLeaf &leaf);
+	IntLeaf mult(const IntLeaf &leaf) const;
 
 	IntLeaf &operator*=(const IntLeaf &leaf);
 	IntLeaf operator*(const IntLeaf &leaf) const;
@@ -19,5 +34,10 @@ public:
 
 	virtual std::vector<char> toVector(void) const;
 	virtual int32_t getLength(void) const;
+
+	std::string toString(void) const;
+
+private:
+	mpz_class data;
 };
 
