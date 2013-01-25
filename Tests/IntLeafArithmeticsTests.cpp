1fa4856718614ba8801722ff4af7d6f137bc18da
Erik Larsson <erikl3@kth.se>
2013-01-25 10:36:06 +0100

    Added arithmetics test for IntLeaf

---
+++ Tests/IntLeafArithmeticsTests.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+
+#include "BaseNode.h"
+#include "Node.h"
+#include "Leaf.h"
+
+TEST(ArithmeticTests, SingleByteAddition)
+{
+    
+}
+
+
+TEST(ArithmeticTests, PositiveAddition) 
+{
+
+}
+
