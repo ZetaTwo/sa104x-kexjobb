2a02810af80ed24809825f30a0e1b97f6fc0b7a0
Erik Larsson <erikl3@kth.se>
2013-02-07 13:44:50 +0100

    Added ProofOfShuffle

---
+++ Verifier/ProofOfShuffle.cpp
@@ -0,0 +1,23 @@
+#include "ProofOfShuffle.h"
+
+bool proofOfShuffle(proofStruct &pfStr, const Node &tau_pos, const Node &sigma_pos)
+{
+    // Step 1
+
+    // a) assert that pfStr->mu is array of Pdersen commitments in pfStr->Gq
+    
+    // b) assert that sigma_pos is a Node (B, A', C', D', F'), where
+    // A',C',D' is in G_q, F' is in Cw and B and B' are arrays of N
+    // elements in Gq
+
+    // c) assert that sigma_pos is a Node (kA, kB, kC, kD, kE, kF),
+    // where kA, kC, kD, kF is in Z_q, kB is an array of N elements in
+    // Rw and kE is an array of N elements in G_q
+
+    // Step 2
+
+    
+
+
+    return true;
+} 
---
+++ Verifier/ProofOfShuffle.h
@@ -0,0 +1,10 @@
+#ifndef VERIFIER_PROOFOFSHUFFLE_H
+#define VERIFIER_PROOFOFSHUFFLE_H
+
+#include "Node.h"
+
+bool proofOfShuffle(proofStruct &pfStr, const Node &tau_pos, const Node &sigma_pos);
+
+#endif
+
+
