c15740dbe7a19aaf992bc4b7d187333be9010f95
Erik Larsson <erikl3@kth.se>
2013-02-07 09:07:50 +0100

    Added files for proofStruct and shared functions

---
+++ Verifier/Utilities.cpp
@@ -0,0 +1,11 @@
+#include "Utilities.h"
+
+bool isElemOf(const Node &group, const IntLeaf &elem)
+{
+    return true;
+}
+
+bool isPedersenCommitment(const Node &group, const IntLeaf &elem)
+{
+    return true;
+}
---
+++ Verifier/Utilities.h
@@ -0,0 +1,28 @@
+#ifndef VERIFIER_UTILITIES_H
+#define VERIFIER_UTILITIES_H
+
+#include "Node.h"
+#include "IntLeaf.h"
+
+struct proofStruct
+{
+    const IntLeaf *rho;
+    const int N;
+    int nE;
+    int nR;
+    int nV;
+    //PRG
+    const Node *Gq;
+    const Node *Rw;
+    const Node *Cw;
+    const Node *Mw;
+    const Node *pk;
+    const Node *w;
+    const Node *w_prime;
+    const Node *mu;
+};
+
+bool isElemOf(const Node &group, const IntLeaf &elem);
+bool isPedersenCommitment(const Node &group, const IntLeaf &elem);
+
+#endif
