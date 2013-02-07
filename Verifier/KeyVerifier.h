1ec9a926de127cee5d1179bf315149aab923b52f
Erik Larsson <erikl3@kth.se>
2013-02-07 09:06:05 +0100

    Added include guard

--- Verifier/KeyVerifier.h
+++ Verifier/KeyVerifier.h
@@ -1,3 +1,6 @@
+#ifndef VERIFIER_KEYVERIFIER_H
+#define VERIFIER_KEYVERIFIER_H
+
 #include "Node.h"
 #include <string>
 
@@ -14,3 +17,5 @@ bool isPartialSecretKey(IntLeaf &leaf);
 const std::string FULL_PUBLIC_KEY_FILE_NAME = "FullPublicKey.bt";
 const std::string PARTIAL_PUBLIC_KEY_FILE_PREFIX = "PublicKey";
 const std::string PARTIAL_SECRET_KEY_FILE_PREFIX = "SecretKey";
+
+#endif
