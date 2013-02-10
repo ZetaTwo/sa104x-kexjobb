cd8b6d827f7043a41810bc96e773f62f8cd25cf5
Erik Larsson <erikl3@kth.se>
2013-02-10 19:06:01 +0100

    Added isElemOf function to decide if IntLeaf is in Z_n

--- Verifier/Utilities.cpp
+++ Verifier/Utilities.cpp
@@ -2,8 +2,27 @@
 
 bool isElemOf(const Node &group, const IntLeaf &elem)
 {
+    const IntLeaf &p = static_cast<const IntLeaf &>(group.getChild(0));
+    const IntLeaf &q = static_cast<const IntLeaf &>(group.getChild(1));
+
+    if(!(elem < p) || !(elem > IntLeaf(0)))
+	return false;
+
+    if(elem.expMod(q, p) != IntLeaf(1))
+	return false;
+
     return true;
 }
+
+bool isElemOf(const IntLeaf &Zn, const IntLeaf &elem)
+{
+    if(!(elem < Zn) || elem < IntLeaf(0))
+	return false;
+
+    return true;
+}
+
+
 
 bool isPedersenCommitment(const Node &group, const IntLeaf &elem)
 {
