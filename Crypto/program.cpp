720b9983736f9a207895203cbefa8f19b93ff56f
Calle Svensson <calle.svensson@zeta-two.com>
2013-02-11 00:41:54 +0100

    SHA256 test.

--- Crypto/program.cpp
+++ Crypto/program.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
+#include <string>
 
-#include <openssl\sha.h>
+#include <openssl/sha.h>
 
 int main() {
 
-	unsigned char message[] = "My message";
-	unsigned char *hash = new unsigned char[SHA_DIGEST_LENGTH];
+	std::string message = "My message";
+	unsigned char *hash = new unsigned char[SHA256_DIGEST_LENGTH];
 
-	SHA1(message, 10, hash);
+	SHA256((unsigned char*)message.c_str(), message.length(), hash);
 
 	std::cout << message << std::endl;
-	for (int i = 0; i < 20; i++) {
+	for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
         printf("%02x ", hash[i]);
     }
 
