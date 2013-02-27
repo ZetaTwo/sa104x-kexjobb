990ed45d370931b5a6e21267e143970d6ec9370a
Calle Svensson <calle.svensson@zeta-two.com>
2013-02-27 16:15:31 +0100

    Completed random array

--- Crypto/RandomArray.cpp
+++ Crypto/RandomArray.cpp
@@ -1,9 +1,24 @@
 #include "RandomArray.h"
 
-Node RandomArray(Node Gq, unsigned int Nprime, PRG &prg, unsigned int Nr) {
+Node RandomArray(Node Gq, unsigned int Nprime, std::vector<unsigned char> (*hash)(std::vector<unsigned char> data), std::vector<unsigned char> seed, unsigned int Nr) {
 
-	prg.next();
-	unsigned int Nq = Gq.getChild(0).getLength() * 8;
+	IntLeaf p = static_cast<IntLeaf &>(Gq.getChild(0));
+	IntLeaf q = static_cast<IntLeaf &>(Gq.getChild(1));;
 
-	return Node();
+	unsigned int Nq = p.getLength() * 8;
+	PRG prg(hash, seed, Nq+Nr);
+	IntLeaf mod(2);
+	mod.expTo(Nq+Nr);
+
+
+	Node result;
+	for (int i = 0; i < Nprime; i++)
+	{
+		IntLeaf child = prg.next();
+		child.modTo(mod);
+		child.expToMod((p + (-1)).mult(q.inverse(p)), p);
+		result.addChild(child);
+	}
+
+	return result;
 }
--- Crypto/RandomArray.h
+++ Crypto/RandomArray.h
@@ -1,3 +1,3 @@
 #include "PRG.h"
 
-Node RandomArray(Node Gq, unsigned int Nprime, PRG &prg, unsigned int Nr);
+Node RandomArray(Node Gq, unsigned int Nprime, std::vector<unsigned char> (*hash)(std::vector<unsigned char> data), std::vector<unsigned char> seed, unsigned int Nr);
