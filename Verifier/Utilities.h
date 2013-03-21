2cbf476a0badb7932501df5b0c24d611c12a811e
Erik Larsson <erikl3@kth.se>
2013-03-21 17:03:58 +0100

    Added width and lambda to proofStruct

--- Verifier/Utilities.h
+++ Verifier/Utilities.h
@@ -10,6 +10,8 @@ struct proofStruct
 {
     IntLeaf rho;
     unsigned int N;
+    unsigned int lambda;
+    unsigned int width;
     unsigned int nE;
     unsigned int nR;
     unsigned int nV;
@@ -17,14 +19,13 @@ struct proofStruct
     bytevector (*hash)(bytevector);
     Node Gq;
     Node Rw;
-    Node Cw;
-    Node Mw;
     Node pk;
     Node y;
     Node x;
 };
 
 bool isElemOf(const Node &group, const IntLeaf &elem);
+bool isElemOf(const Node &group, unsigned int width, const Node &elem);
 bool isElemOf(const IntLeaf &Zn, const IntLeaf &elem);
 bool isPedersenCommitment(const Node &group, const IntLeaf &elem);
 
