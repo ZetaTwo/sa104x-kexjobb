601c22ee28df575b83ad36e50b482f77d049e7e5
Erik Larsson <erikl3@kth.se>
2013-03-26 16:52:40 +0100

    Corrected small errors

--- Verifier/Verifier.cpp
+++ Verifier/Verifier.cpp
@@ -40,33 +40,38 @@ int Verifier(string protinfo, string directory,
     string Sh;
     string sid;
     string pGroup;
-	string Sprg;
-    IntLeaf Nv(0, 4);
-	IntLeaf Nr(0, 4);
-    IntLeaf Ne(0, 4);
-
-    int lambda;
+    string Sprg;
+    unsigned int wDefault;
+    pfStr.nV = IntLeaf(0, 4);
+    pfStr.nR = IntLeaf(0, 4);
+    pfStr.nE = IntLeaf(0, 4);
     
+    // Set values in proofStruct
     try {
 	versionProt = root_node->first_node("version")->value();
 	sid = root_node->first_node("sid")->value();
 	string k = root_node->first_node("nopart")->value();
 	
 	string strLambda = root_node->first_node("thres")->value();
-	lambda = atol(strLambda.c_str());
+	pfStr.lambda = atol(strLambda.c_str());
 	
 	pGroup = root_node->first_node("pgroup")->value();
 	getGroupFromString(pfStr, pGroup);
 
-	string strNv = root_node->first_node("cbitlen")->value();
-	Nv = atol(strNv.c_str());
+	string strNv = root_node->first_node("cbitlenro")->value();
+	pfStr.nV = atol(strNv.c_str());
 	
-	string strNe = root_node->first_node("vbitlen")->value();
-	Ne = atol(strNe.c_str());
+	string strNe = root_node->first_node("vbitlenro")->value();
+	pfStr.nE = atol(strNe.c_str());
+
+	string strNr = root_node->first_node("statdist")->value();
+	pfStr.nR = atol(strNr.c_str());
 	
 	Sprg = root_node->first_node("prg")->value();
 	Sh = root_node->first_node("rohash")->value();
-	string Cw = root_node->first_node("width")->value();
+	string strwidth = root_node->first_node("width")->value();
+        wDefault = atol(strwidth.c_str());
+
     } catch (...) {
 		return false;
     }
@@ -76,9 +81,9 @@ int Verifier(string protinfo, string directory,
     //Step 2
     string versionProof;
     string typeStr;
-	RunMode type;
+    RunMode type;
     string auxsid;
-	int width;
+    int width;
     string widthStr;
     
     string filename = directory + "/version";
@@ -86,74 +91,80 @@ int Verifier(string protinfo, string directory,
     datafile >> versionProof;
     if(versionProof != versionProt) { return false; }
     
+    //Determine type
     filename = directory + "/type";
     datafile.open(filename.c_str());
     datafile >> typeStr;
-	if(typeStr == "mixing") { type = MIX; }
-	if(typeStr == "shuffling") { type = SHUFFLE; }
-	if(typeStr == "decryption") { type = DECRYPT; }
+    if(typeStr == "mixing") { type = MIX; }
+    if(typeStr == "shuffling") { type = SHUFFLE; }
+    if(typeStr == "decryption") { type = DECRYPT; }
 
+    // Make sure that type is the expected type
     if(type != typeExp) { return false; }
     
+    // Check auxid
     filename = directory + "/auxsid";
     datafile.open(filename.c_str());
     datafile >> auxsid;
     if(auxsid != auxsidExp) { return false; }
     
+    // Check width parameters
     filename = directory + "/width";
     datafile.open(filename.c_str());
     datafile >> widthStr;
-	width = atoi(widthStr.c_str());
-    if(wExp != -1 && wExp != width) { return false; }
+    pfStr.width = atoi(widthStr.c_str());
+    if(wExp != -1 && wExp != pfStr.width) { return false; }
+    if(wExp == -1 && width != wDefault) { return false; }
     
     //Step 3
-	if(Sh == "SHA-256") {
-		pfStr.hash = H_SHA256;
-	} else if(Sh == "SHA-384") {
-		pfStr.hash = H_SHA384;
-	} else if(Sh == "SHA-512") {
-		pfStr.hash = H_SHA512;
-	} else {
-		return false;
-	}
+    if(Sh == "SHA-256") {
+	pfStr.hash = H_SHA256;
+	pfStr.nH = 256;
+    } else if(Sh == "SHA-384") {
+	pfStr.hash = H_SHA384;
+	pfStr.nH = 384;
+    } else if(Sh == "SHA-512") {
+	pfStr.hash = H_SHA512;
+	pfStr.nH = 512;
+    } else {
+	return false;
+    }
     
     //Step 4
     //rho = H(node(versionProof, sid + "." auxsid, omega, Ne, Nr, Nv, Gq, Sprg, Sh))
-	DataLeaf rho_version;
-	bytevector version_data = rho_version.getData();
-	version_data.insert(version_data.end(), versionProof.begin(), versionProof.end());
-
-	DataLeaf rho_id;
-	bytevector id_data = rho_id.getData();
-	id_data.insert(id_data.end(), sid.begin(), sid.end());
-	id_data.push_back('.');
-	id_data.insert(id_data.end(), auxsid.begin(), auxsid.end());
-	
-	Node rho;
-	rho.addChild(rho_version);
-	rho.addChild(rho_id);
-	rho.addChild(IntLeaf(width, 4));
-	rho.addChild(Ne);
-	rho.addChild(Nr);
-	rho.addChild(Nv);
-	rho.addChild((DataLeaf)pGroup);
-	rho.addChild((DataLeaf)Sprg);
-	rho.addChild((DataLeaf)Sh);
-
-	pfStr.rho = (IntLeaf)pfStr.hash(rho.serialize());
+    DataLeaf rho_version;
+    bytevector version_data = rho_version.getData();
+    version_data.insert(version_data.end(), versionProof.begin(), versionProof.end());
+    
+    DataLeaf rho_id;
+    bytevector id_data = rho_id.getData();
+    id_data.insert(id_data.end(), sid.begin(), sid.end());
+    id_data.push_back('.');
+    id_data.insert(id_data.end(), auxsid.begin(), auxsid.end());
+    
+    Node rho;
+    rho.addChild(rho_version);
+    rho.addChild(rho_id);
+    rho.addChild(IntLeaf(pfStr.width, 4));
+    rho.addChild(pfStr.nE);
+    rho.addChild(pfStr.nR);
+    rho.addChild(pfStr.nV);
+    rho.addChild((DataLeaf)pGroup);
+    rho.addChild((DataLeaf)Sprg);
+    rho.addChild((DataLeaf)Sh);
+    
+    pfStr.rho = (IntLeaf)pfStr.hash(rho.serialize());
     
     //Step 5
-	
-    Node key_node;
-    
-    if(!keyVerifier(lambda, pfStr))
+        
+    if(!keyVerifier(pfStr))
     {
 	return false;
     }
     
-
+    
     //Step 6 Read lists
-        
+    
     Node L0;
     Node Llambda;
 
